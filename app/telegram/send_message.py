bf3ecd01afc8fce9f61c0587888a2f44a971eb5c
ali rezazadeh <lirezazadeh77@gmail.com>
2022-10-31 02:16:33 +0330

    beautify message

--- app/telegram/send_message.py
+++ app/telegram/send_message.py
@@ -3,12 +3,17 @@ import telegram
 from app.config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
 
-def send_message(text):
+def send_message(hook_data):
     telegram_token = TELEGRAM_TOKEN
     telegram_chat_id = TELEGRAM_CHAT_ID
     _bot = telegram.Bot(token=telegram_token)
     str_message = ""
-    for key, value in text.items():
+    beautify = {"servers":[], 'message':''}
+    for instance in hook_data['evalMatches']:
+        beautify['servers'].append(instance['instance'] + instance['value'])
+    beautify['message'] = hook_data['message']
+
+    for key, value in hook_data.items():
         str_message += f'<em><b>{key.replace("_"," ")}:</b></em>\n' \
                        f'{value}\n'
 
