3aaf25e43d8a8c284e39069dc70a7ad8646eb6ab
ali rezazadeh <lirezazadeh77@gmail.com>
2022-10-31 01:18:51 +0330

    setup format for message

--- app/telegram/send_message.py
+++ app/telegram/send_message.py
@@ -7,4 +7,9 @@ def send_message(text):
     telegram_token = TELEGRAM_TOKEN
     telegram_chat_id = TELEGRAM_CHAT_ID
     _bot = telegram.Bot(token=telegram_token)
+    str_message = ""
+    for key, value in text.items():
+        str_message += f'<em><b>{key.replace("_"," ")}:</b></em>\n' \
+                       f'{value}\n'
+
     _bot.sendMessage(chat_id=telegram_chat_id, text=text, parse_mode=telegram.ParseMode.HTML)
