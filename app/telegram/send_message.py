bd8adbbf1e1548ea61dbfb4af7f56ef01ec38a1f
ali rezazadeh <lirezazadeh77@gmail.com>
2022-10-31 01:16:37 +0330

    fix chat id name

--- app/telegram/send_message.py
+++ app/telegram/send_message.py
@@ -7,4 +7,4 @@ def send_message(text):
     telegram_token = TELEGRAM_TOKEN
     telegram_chat_id = TELEGRAM_CHAT_ID
     _bot = telegram.Bot(token=telegram_token)
-    _bot.sendMessage(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.HTML)
+    _bot.sendMessage(chat_id=telegram_chat_id, text=text, parse_mode=telegram.ParseMode.HTML)
